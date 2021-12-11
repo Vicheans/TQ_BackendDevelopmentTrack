1b79aa0c44304d6612ce10e6c66addca6b8d90f0
Joshua Gbogodor <40539860+Lilzj@users.noreply.github.com>
2021-12-11 14:22:00 +0100

    Create Assignment4.js

---
+++ Assignment4.js
@@ -0,0 +1,5 @@
+/**Write a javacript function that will ask a user for a number and also a range, 
+use the number collected from the user to create a multiplication table using 
+the range provided by the user to end the multiplication.
+
+Use the for loop and the whhile loop for this exercise**/
