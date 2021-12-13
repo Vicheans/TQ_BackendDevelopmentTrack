832b59b9429386bfd6f2705ec310bd45a5101efa
Ade_B <54278310+Ade-Bq@users.noreply.github.com>
2021-12-13 15:03:43 +0100

    convering wholeNumber to binary

--- assignment.js
+++ assignment.js
@@ -1,8 +1,10 @@
 //ADE BUKOLA
 // Write a function to convert a whole number to binary
-
-
-
+let wholeNumber = 4
+function binary() {
+    console.log(wholeNumber.toString(2))
+}
+binary()
 
 // Adedoyin
 //Write a function to add up a number from zero to a given number
